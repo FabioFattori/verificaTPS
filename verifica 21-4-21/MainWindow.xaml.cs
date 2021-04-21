bd92db3621188a2d2e01fde60b9aacffcad5eb7f
fabio.fattori <fabio.fattori@PC7322>
2021-04-21 11:14:49 +0200

    programma finito!

--- verifica 21-4-21/MainWindow.xaml.cs
+++ verifica 21-4-21/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.IO;
 
 namespace verifica_21_4_21
 {
@@ -20,9 +22,118 @@ namespace verifica_21_4_21
     /// </summary>
     public partial class MainWindow : Window
     {
+        int lunghezzaStringa;
+        List<string> caratteriPossibili;
+        Random r;
+        int i ;
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random();
+            caratteriPossibili = new List<string>();
+            btn_estrai.Visibility = Visibility.Hidden;
+           
+        }
+
+        public void LeggiFile()
+        {
+            using(StreamReader sr=new StreamReader("file.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    caratteriPossibili.Add(sr.ReadLine());
+                }
+            }
+        }
+
+        private async void Sorteggio()
+        {
+            await Task.Run(() =>
+            {
+                int sorteggiato;
+                i = 0;
+                while (i<lunghezzaStringa)
+                {
+                    sorteggiato = r.Next(1, caratteriPossibili.Count);
+                    Thread.Sleep(550);
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        lbl_letteraCheCambia.Content = caratteriPossibili[sorteggiato];
+                    }));
+                    
+                }
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    btn_estrai.IsEnabled = false;
+                }));
+                
+            });
+        }
+
+        private void btn_estrai_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_stringaCheVieneComposta.Content += (string)lbl_letteraCheCambia.Content;
+            i++;
+        }
+
+        
+        private async void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            btn_start.IsEnabled = false;
+            LeggiFile();
+            
+            lunghezzaStringa = Convert.ToInt32(txt_lunghezzaParola.Text);
+
+            Sorteggio();
+            btn_estrai.Visibility = Visibility.Visible;
+            
+
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    AggiungiInLista();
+                }
+            });
+                
+        }
+
+        private async void txt_lunghezzaParola_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    try
+                    {
+                        lunghezzaStringa = Convert.ToInt32(txt_lunghezzaParola.Text);
+                    }
+                    catch
+                    {
+                        lunghezzaStringa = 1;
+                    }
+                }));
+                
+            });
+
+            
+        }
+
+        public void AggiungiInLista()
+        {
+            if (i == lunghezzaStringa)
+            {
+                
+                i = 0;
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    
+                    lst_listaDiParole.Items.Add(lbl_stringaCheVieneComposta.Content);
+                    lbl_stringaCheVieneComposta.Content = "";
+                    
+                }));
+                
+            }
         }
     }
 }
